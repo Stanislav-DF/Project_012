f67e4d5efeecb1aac31eb87bde111e99d99f9427
Stanislav <example@gmail.com>
2022-05-08 18:26:39 +0200

    add homepractic 015

--- script.js
+++ script.js
@@ -1,23 +1,5 @@
 'use strict'
 
-/* let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", " ");
-let lastFilm = prompt("Один из последних просмотренных фильмов?", " ");
-let grade = +prompt("На сколько оцените его?", " ")
-
-let personalMovieDB = {
-    count: numberOfFilms,
-    movies:{
-        film: lastFilm,
-        rating: grade,
-    },
-    actors:{},
-    genres: [],
-    privat:false
-
-}
-
-console.log(personalMovieDB); */
-
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 const personalMovieDB = {
@@ -28,13 +10,47 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ' '),
-      b = prompt('На сколько оцените его?', ' '),
-      c = prompt('Один из последних просмотренных фильмов?', ' '),
-      d = prompt('На сколько оцените его?', ' ');
+// 1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
+for (let i = 0; i <= 1 ; i++){
+    let a = prompt('Один из последних просмотренных фильмов?', ' '),
+        b = prompt('На сколько оцените его?', ' ');
+        
+    
+/* 2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
+возвращаем пользователя к вопросам опять */
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+        if (a === " " || b === " "){
+            alert("Напишите название фильма!");
+            a = prompt('Один из последних просмотренных фильмов?', ' '),
+            b = prompt('На сколько оцените его?', ' ');
+        } else if(a.length === 5){
+            alert("Слишком длинное название");
+        } else if (a == false){
+            alert('Cancel');
+        }
+    personalMovieDB.movies[a] = b;
+    
+}
+
+/* 3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
+"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка" */
+
+if (personalMovieDB.count < 10){
+    alert("Просмотренно довольно мало фильмов");
+} else if (personalMovieDB.count > 10 && personalMovieDB.count < 30){
+    alert(" Вы класический зритель");
+} else if (personalMovieDB.count > 30){
+    alert("Ва киноман!");
+}
+
+
+/*  if (a === false){
+    alert ("Введите название фильма");
+     let a = prompt('Один из последних просмотренных фильмов?', ' '),
+        b = prompt('На сколько оцените его?', ' '); 
+}  */
+ 
 
 console.log(personalMovieDB);
-
