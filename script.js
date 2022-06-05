1bc01ea916e3a6ec11ec950568e531fd8c8a8a26
Stanislav <example@gmail.com>
2022-06-05 16:37:21 +0200

    Practic 024 for Dmitriy

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 'use strict'
 
 let numberOfFilms;
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies:{},
+    actors:{},
+    genres:[],
+    privat: false
+};
+
+// 1) У нас уже есть рабочее приложение, состоящее из отдельных функций. Представьте, что
+// перед вами стоит задача переписать его так, чтобы все функции стали методами объекта personalMovieDB
+// Такое случается в реальных продуктах при смене технологий или подхода к архитектуре программы
+
+
 
 function start() {
     numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
@@ -10,15 +23,8 @@ function start() {
     }
 }
 
-start();
-
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies:{},
-    actors:{},
-    genres:[],
-    privat: false
-};
+personalMovieDB.start = start;
+personalMovieDB.start();
 
 
 
@@ -36,7 +42,8 @@ function rememberMyFilms () {
         }              
     }
 }
-rememberMyFilms ();
+personalMovieDB.rememberMyFilms = rememberMyFilms;
+personalMovieDB.rememberMyFilms ();
 
 /*  let i = 0;
 while(i < 2){
@@ -66,10 +73,15 @@ function detectPersonalLevel () {
         console.log('Произошла ошибка');
     }
 }
-detectPersonalLevel ();
+
+personalMovieDB.detectPersonalLevel = detectPersonalLevel;
+personalMovieDB.detectPersonalLevel ();
 
 // 2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
 // false - выводит в консоль главный объект программы
+
+// 2.1) Создать метод toggleVisibleMyDB, который при вызове будет проверять свойство privat. Если оно false - он
+// переключает его в true, если true - переключает в false. Протестировать вместе с showMyDB.
 
 function showMyDB () {
     if (personalMovieDB.privat == false) {
@@ -78,18 +90,46 @@ function showMyDB () {
         alert('Access is denied');
     }
 }
-showMyDB ();
+personalMovieDB.showMyDB = showMyDB;
+personalMovieDB.showMyDB ();
+
+function toggleVisibleMyDB () {
+    if (personalMovieDB.privat == false){
+        personalMovieDB.privat = true;
+    } else if (personalMovieDB.privat == true) {
+        personalMovieDB.privat = false 
+    }
+}
+
+personalMovieDB.toggleVisibleMyDB = toggleVisibleMyDB;
+personalMovieDB.toggleVisibleMyDB();
 
 /* 3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
 "Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
 genres */
 
+// 3.1) В методе writeYourGenres запретить пользователю нажать кнопку "отмена" или оставлять пустую строку. 
+// Если он это сделал - возвращать его к этому же вопросу. После того, как все жанры введены - 
+// при помощи метода forEach вывести в консоль сообщения в таком виде:
+// "Любимый жанр #(номер по порядку, начиная с 1) - это (название из массива)"
+
 function writeYourGenres() {
-    for (let i = 0; i < 3; i++){
+    for (let i = 1; i <= 3; i++){
         let a = prompt(`Ваш любимый жанр под номером ${i}`, " ");
-        personalMovieDB.genres[i] = a; 
-    }
+        personalMovieDB.genres[i - 1] = a;
+        if ( a == " " && a != false) {
+            i--;
+        }else {
+            console.log("I'm here")
+        }
+    }       
 }
-writeYourGenres();
+personalMovieDB.writeYourGenres = writeYourGenres;
+personalMovieDB.writeYourGenres ();
+
+
+
+
+
 
 
